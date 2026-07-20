db27d302636a5282a472f79a9ec39443257cf41d
Farhan-Sathik <146457629+Farhan-Sathik@users.noreply.github.com>
2026-07-20 12:07:30 +0530

    Task 2 Completed

---
+++ Day 4/task2.js
@@ -0,0 +1,27 @@
+let a=100;
+let b=20;
+console.log("The value of a ",a);
+console.log("The value of b",b);
+//Arithmetic operator
+console.log("The value of a+b ",b);
+console.log("The value of a-b ",b);
+console.log("The value of a*b ",b);
+console.log("The value of a/b ",b);
+console.log("The value of a%b ",b);
+console.log("The value of a**b ",b);
+
+//Relational operator
+console.log("The value of a>b ",b);
+console.log("The value of a<b ",b);
+console.log("The value of a==b ",b);
+console.log("The value of a===b ",b);
+console.log("The value of a!=b ",b);
+console.log("The value of a!==b ",b);
+
+//Assignment operator
+console.log("The value of a+=b ",a+=b);
+console.log("The value of a-=b ",a+=b);
+
+//Terinary Operator 
+var result=a>b ? a:b;
+console.log("The gratest number is ",result);
