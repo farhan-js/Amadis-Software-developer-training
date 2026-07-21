a593689bdffdcfbfe9c55d8eebb1f1bd3cc3cda4
Farhan-Sathik <146457629+Farhan-Sathik@users.noreply.github.com>
2026-07-21 17:59:57 +0530

    Task 3 completed

---
+++ Day 5/Task 3.js
@@ -0,0 +1,11 @@
+let student={
+    "name":"Farhan",
+    "id":102,
+    "class":12
+}
+console.log(student.name);
+console.log(student["name"]);
+let keyStudent=Object.keys(student);
+let valueStudent=Object.values(student);
+console.log(keyStudent);
+console.log(valueStudent);
