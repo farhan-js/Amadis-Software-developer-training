2173188f644961200366d28590529a0566070d70
Farhan-Sathik <146457629+Farhan-Sathik@users.noreply.github.com>
2026-07-23 12:58:00 +0530

    Let var and const completed

---
+++ Day 7/letVarConst.js
@@ -0,0 +1,25 @@
+id =5;
+function example() {
+
+    var id = 10;
+    let age = 21;
+    const country = "India";
+
+    console.log(id);  
+    console.log(age);      
+    console.log(country);   
+
+
+    company = "Microsoft";   
+    age = 22;                
+    // country = "USA";    
+
+    console.log(id); 
+    console.log(age);   
+    console.log(country); 
+}
+
+example();
+console.log(id);
+// console.log(age);   
+// console.log(country); 
