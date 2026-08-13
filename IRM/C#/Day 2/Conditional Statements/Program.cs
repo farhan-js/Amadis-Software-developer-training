2562e7cc258147cbe5af43a6bcebc7eb02d08fd3
Farhan-Sathik <146457629+Farhan-Sathik@users.noreply.github.com>
2026-08-13 15:47:30 +0530

    Conditional Statements

---
+++ IRM/C#/Day 2/Conditional Statements/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+class Conditional
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Enter your age");
+        int age =int.Parse(Console.ReadLine());
+
+        if (age >= 18)
+        {
+            Console.WriteLine("Eligible to Vote");
+        }
+
+        Console.WriteLine("Enter a number:");
+        int num=int.Parse(Console.ReadLine()!);
+        if (num > 0)
+        {
+            Console.WriteLine("Positive number");
+        }
+        else if (num < 0)
+        {
+            Console.WriteLine("Negative number");
+        }
+        else
+        {
+            Console.WriteLine("Number is Zero");
+        }
+        Console.WriteLine("Enter a number for month");
+        int month= int.Parse(Console.ReadLine()!);
+        switch (month){
+            case 1:
+                Console.WriteLine("January");
+                break;
+            case 2:
+                Console.WriteLine("February");
+                break;
+            
+            case 3:
+                Console.WriteLine("March");
+                break;
+            
+            case 4:
+                Console.WriteLine("April");
+                break;
+            
+            case 5:
+                Console.WriteLine("May");
+                break;
+            
+            case 6:
+                Console.WriteLine("June");
+                break;
+            
+            case 7:
+                Console.WriteLine("July");
+                break;
+            
+            case 8:
+                Console.WriteLine("August");
+                break;
+            
+            case 9:
+                Console.WriteLine("September");
+                break;
+            case 10:
+                Console.WriteLine("October");
+                break;
+            case 11:
+                Console.WriteLine("November");
+                break;
+            case 12:
+                Console.WriteLine("December");
+                break;
+            default:
+                Console.WriteLine("Enter a valid Input");
+                break;
+            
+
+        }
+    }
+}
